c1091ddf228d647cc9c1b1dd349e3a070151cd05
alyonabakirova <86947336+alyonabakirova@users.noreply.github.com>
2021-07-22 17:20:33 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#for dadata
+TOKEN_DADATA = "d6c4e6cc7f93ad1ff6b8e8c76528d501135eb7ec"
+URL_DADATA = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party"
+
+#for telegram bot
+TOKEN_BOT = '1806208816:AAEZaHOsy95Z3Yf347rTRzcpBO3Tg8g1g0w'
+
+#for api leasing-trade
+TOKEN_LT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMWM2NGNmYTdkZDYyNzVhYjBmMjgxMyIsImlhdCI6MTYyNjg2ODE4OH0.qCmS7c3JbVkejpGXn1lCnDaz7WPApRtF9sEsCtlYLnU'
+URL_LT = 'https://api.leasing-trade.ru:4000/graphql'
